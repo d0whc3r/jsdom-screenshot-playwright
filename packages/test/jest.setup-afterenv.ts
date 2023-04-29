f90ef377445191662c232fa152c17422daef432a
d0whc3r <1378986+d0whc3r@users.noreply.github.com>
2023-04-29 13:41:24 +0200

    fix: use src code in tests

--- packages/test/jest.setup-afterenv.ts
+++ packages/test/jest.setup-afterenv.ts
@@ -1,7 +1,7 @@
+import { close, start } from '../lib/src'
 import '@testing-library/jest-dom'
 import 'core-js'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
-import { close, start } from 'jsdom-screenshot-playwright'
 
 expect.extend({ toMatchImageSnapshot })
 
