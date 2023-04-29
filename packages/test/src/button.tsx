c5ae5e1940131f36acb58bc8f592c0dd99a9f2f4
d0whc3r <1378986+d0whc3r@users.noreply.github.com>
2023-04-29 20:07:11 +0200

    fix: add font config

--- packages/test/src/button.tsx
+++ packages/test/src/button.tsx
@@ -11,7 +11,20 @@ export default function Button({ children, variant = 'primary' }: ButtonProps) {
     color: variant === 'primary' ? 'white' : 'black',
     height: '30px',
     width: '120px',
+    fontFamily: 'Roboto, arial, sans-serif',
+    fontSize: '16px',
+    fontWeight: 400,
   }
 
-  return <button style={style}>{children}</button>
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+        rel="stylesheet"
+      />
+      <button style={style}>{children}</button>
+    </>
+  )
 }
