81e465b45612de042001ecdf96b4b6bcccff8554
d0whc3r <1378986+d0whc3r@users.noreply.github.com>
2023-04-29 19:56:59 +0200

    fix: test new config

--- packages/test/src/button.tsx
+++ packages/test/src/button.tsx
@@ -9,9 +9,11 @@ export default function Button({ children, variant = 'primary' }: ButtonProps) {
   const style: CSSProperties = {
     backgroundColor: variant === 'primary' ? 'blue' : 'red',
     color: variant === 'primary' ? 'white' : 'black',
-    height: '25px',
-    width: '100px',
-    fontFamily: 'Roboto, sans-serif',
+    height: '30px',
+    width: '120px',
+    fontFamily: 'Roboto, arial, sans-serif',
+    fontSize: '16px',
+    fontWeight: 400,
   }
 
   return (
@@ -19,7 +21,7 @@ export default function Button({ children, variant = 'primary' }: ButtonProps) {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
         rel="stylesheet"
       />
       <button style={style}>{children}</button>
