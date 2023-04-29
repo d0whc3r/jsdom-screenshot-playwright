50c81e7f575523f37b643611f53a468d5bbaa8bd
d0whc3r <1378986+d0whc3r@users.noreply.github.com>
2023-04-29 15:20:42 +0200

    fix: consistent design

--- packages/test/src/button.tsx
+++ packages/test/src/button.tsx
@@ -5,9 +5,12 @@ type ButtonProps = {
   variant?: 'primary' | 'secondary'
 }
 
-export default function Button({ children, variant }: ButtonProps) {
+export default function Button({ children, variant = 'primary' }: ButtonProps) {
   const style: CSSProperties = {
     backgroundColor: variant === 'primary' ? 'blue' : 'red',
+    color: variant === 'primary' ? 'white' : 'black',
+    height: '25px',
+    width: '100px',
   }
 
   return <button style={style}>{children}</button>
